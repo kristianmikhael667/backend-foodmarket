a4272a3bc86ac874bd81092442a7fec9e49e9487
kristianmikhael667 <kristianmikhael667@gmail.com>
2022-01-10 23:28:37 +0700

    as

--- resources/views/food/create.blade.php
+++ resources/views/food/create.blade.php
@@ -33,6 +33,7 @@
                                 class="appearance-none block w-full bg-gray-200 text-grey-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                                 id="grid-last-name">
                         </div>
+                        {{-- food --}}
                     </div>
 
                     <div class="flex flex-wrap -mx-3 mb-6">
